3f9c42767374f3ac8abbfb36c6f001c92e6065b1
Roberto Rocuant <r.rocuant4@hotmail.com>
2018-07-07 00:35:28 -0400

    Valor Query
    Tanto cantidad de datos como usuario actual en uso, y simplificacion de select, haciendo un select multiple

---
+++ testcurso/ValorQuery.sql
@@ -0,0 +1,35 @@
+set serveroutput on;
+/*
+--Muestra datos de dual
+select * from dual;
+
+--Muestra cantidad de datos en dual
+select count (*) from dual;
+
+--Muestra que usuario actuo en cada tabla
+select user from dual;
+*/
+
+DECLARE
+    cantidad number;
+    usuario nvarchar2(100);
+    
+
+BEGIN
+    
+    select user into usuario from dual;
+    
+--Con esto se puede saber que usuario es el usado actualmente
+    dbms_output.put_line ('Usuario:' || usuario );
+
+    select count (*) into cantidad from dual;
+
+    dbms_output.put_line ('Cantidad:' || cantidad );
+    
+--Para simplificar ambos select se puede usar
+--select user, count (*) into usuario, cantidad from dual;
+
+    
+    
+END;
+/
