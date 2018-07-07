7983205c7e378dedccedbff4a0a78caf13a4fa83
Roberto Rocuant <r.rocuant4@hotmail.com>
2018-07-06 23:14:11 -0400

    Creacion hola mundo separado
    Creacion de sql unt para hola mundo y concatenacion de textos

---
+++ testcurso/HolaMundo.sql
@@ -0,0 +1,11 @@
+DECLARE
+--Definicion de funciones y variables
+
+
+BEGIN
+
+    dbms_output.put_line ('Hola Mundo');
+    dbms_output.put_line ('Escribir texto aqui. ' || 'Texto concatenado.');
+
+END;
+/
