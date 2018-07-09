2d1cc2a61c76e15ab433bc3da3eee612c3dd4c12
Roberto Rocuant <r.rocuant4@hotmail.com>
2018-07-09 01:19:32 -0400

    Condicion if
    Inicio en condición if basica, y else.

---
+++ testcurso/08_Condicion_01.sql
@@ -0,0 +1,26 @@
+set serveroutput on;
+
+DECLARE
+
+    nota number := 37;
+
+BEGIN
+
+--If con or/and
+    IF ( nota = 39 or nota = 38 ) THEN
+        nota := 40;
+--En caso de ser muchas excepciones usar between
+-- ( nota between 35 and 39 )
+        
+    END IF;
+
+    dbms_output.put_line ( 'Nota: ' || nota );
+    
+    IF ( nota >= 40 ) THEN
+        dbms_output.put_line ( 'Aprobado' );
+    ELSE
+        dbms_output.put_line ( 'Reprobado' );
+    END IF;
+
+END;
+/
